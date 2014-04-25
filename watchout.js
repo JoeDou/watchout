a2cc0fa95e8556daaf6b861809f10837ffc07170
Hack Reactor Students <joe.dou@gmail.com>
2014-04-25 15:37:45 -0700

    Placed enemies on board

--- watchout.js
+++ watchout.js
@@ -1,2 +1,32 @@
-// start slingin' some d3 here.
+var height = 600;
+var width = 600;
+var numCircles = 20;
+
+// Helper functions
+
+var generateCoordinates = function() {
+  var array = [];
+  for (var i = 0; i < numCircles; i++) {
+    array.push(1);
+  }
+  return array;
+};
+
+var arr = generateCoordinates();
+
+// Draw elements in svg element
+
+var svg = d3.select("body").append("svg")
+  .attr("width", width)
+  .attr("height", height)
+  .attr("class", "gameArea");
+
+var enemy = d3.select(".gameArea").selectAll("circle")
+  .data(arr);
+
+ enemy.enter().append("circle")
+  .attr("cx", function(d) { return d*Math.random()*width*0.85;})
+  .attr("cy", function(d) { return d*Math.random()*height*0.85;})
+  .attr("class", "enemy")
+  .attr("r", 10);
 
