b721b8d968d59444d347d0cafe8d9b2d93e805e1
JoeDou <joe.dou@gmail.com>
2014-04-26 16:03:02 -0700

    add pacman eating functionality

--- watchout.js
+++ watchout.js
@@ -1,11 +1,11 @@
 var height = 600;
 var width = 600;
-var numCircles = 20;
+var numCircles = 40;
 var radius = 10;
 var  score = 0;
 var highScore = 0;
 var collisionNum = 0;
-var collisionBool = false;
+var collisionObj = {};
 
 // Manage the score!
 var updateScore = function(){
@@ -26,7 +26,7 @@ var updateCollisions = function() {
 var generateCoordinates = function() {
   var array = [];
   for (var i = 0; i < numCircles; i++) {
-    array.push(1);
+    array.push(i);
   }
   return array;
 };
@@ -65,33 +65,40 @@ function update(data) {
 
 
   enemy.enter().append("circle")
-    .attr("cx", function(d) { return d*Math.random()*width*0.85;})
-    .attr("cy", function(d) { return d*Math.random()*height*0.95;})
+    .attr("cx", function(d) { return Math.random()*width*0.85;})
+    .attr("cy", function(d) { return Math.random()*height*0.95;})
     .attr("class", "enemy")
     .attr("r", radius)
     .attr('filter','url(#image)');
 
   // enter + update
   enemy.transition()
-    .attr("cx", function(d) { return d*Math.random()*width*0.85;})
-    .attr("cy", function(d) { return d*Math.random()*height*0.95;})
+    .attr("cx", function(d) { return Math.random()*width*0.85;})
+    .attr("cy", function(d) { return Math.random()*height*0.95;})
     .duration(1000)
-    .tween(".enemy", function() {
+    .tween(".enemy", function(d) {
       return function(t) {
-        var heroX = hero[0][0].cx.animVal.value;
-        var heroY = hero[0][0].cy.animVal.value;
-        var enemyX = this.cx.animVal.value;
-        var enemyY = this.cy.animVal.value;
-        if( Math.abs(heroX - enemyX) < radius && Math.abs(heroY - enemyY))  {
-          collisionBool = true;
+        var heroX = d3.select(hero[0][0]).attr('cx');
+        var heroY = d3.select(hero[0][0]).attr('cy');
+        var enemyX = d3.select(this).attr('cx');
+        var enemyY = d3.select(this).attr('cy');
+        if( Math.abs(heroX - enemyX) < 2*radius && Math.abs(heroY - enemyY) < 2*radius)  {
+          collisionObj[d] = true;
+          d3.select(this)
+            .remove();
+          //data.pop();
         }
       };
     });
 
-  if (collisionBool){
-    collisionNum++;
+  if (Object.keys(collisionObj).length > 0){
+    for(var keys in collisionObj){
+      collisionNum++;
+    }
+    collisionObj.delete;
+    collisionObj = {};
+
     updateCollisions();
-    collisionBool = false;
     if (score > highScore){
       highScore = score;
       updateHighScore();
@@ -99,8 +106,13 @@ function update(data) {
     score=0;
   }
 
+  //d3.select('.eaten').remove();
+  //data.pop();
+
   // exit
-  enemy.exit().remove();
+  enemy.exit()
+    .remove();
+
 }
 
 // initial display
@@ -109,20 +121,18 @@ update(dataArr);
 var gameboard = d3.select(".gameArea");
 
 var hero = gameboard.selectAll(".hero")
-  .data([{cx: width/2, cy: height/2}]);
+  .data([{cx: width/4, cy: height/4}]);
 
 hero.enter().append("circle")
   .attr("class", "hero")
   .attr("cx", function(d) { return d.cx;})
   .attr("cy", function(d) { return d.cy;})
-  .attr("r",radius)
+  .attr("r",radius*2)
   .attr('filter','url(#pacman)');
 
 
 var dragHero = d3.behavior.drag()
   .on('dragstart', function(){
-    d3.event.sourceEvent.stopPropagation();
-    d3.event.sourceEvent.preventDefault();
   })
   .on('drag', function(d,i){
     d.cx += d3.event.dx;
